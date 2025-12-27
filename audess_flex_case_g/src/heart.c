34387bb0b3567c5c0b3c9cf137e006927f58c38c
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-12-26 19:49:13 -0800

    Add heart timer module and integrate with main loop
    Introduced heart.h and heart.c to provide millisecond timing using SysTick for the S32K344. Updated main.c to use the new timer for task scheduling, added a blinky task, and initialized required GPIOs for output.

---
+++ audess_flex_case_g/src/heart.c
@@ -0,0 +1,19 @@
+#include "heart.h"
+
+// Reading the data sheet it seems to be the clock for the S32K344 is 48MHz
+#define MS_TICKS 48000UL
+
+volatile uint32_t tickCount = 0;
+
+void heartInit() {
+	NVIC_EnableIRQ(SysTick_IRQn);
+	SysTick_Config(MS_TICKS);
+}
+
+uint32_t elapsedMs() {
+	return tickCount;
+}
+
+void SysTick_Handler() {
+	tickCount++;
+}
